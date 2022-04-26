75d1a18bcf96679032e5c72a17fdc7308e88d2ec
TinaSzucs <puncs17@gmail.com>
2022-04-26 18:34:07 +0100

    merged DateOfBirth & DateOfJoining into DateOf updated Salary

--- src/main/java/model/validate/Salary.java
+++ src/main/java/model/validate/Salary.java
@@ -1,7 +1,18 @@
 package model.validate;
 
-public class Salary {
-    public boolean validateData(String empID){
+import java.util.regex.Pattern;
 
+public abstract class Salary implements ValidateData {
+    private final Pattern salaryPattern = Pattern.compile("\\d+");
+    public boolean validateData(String salary){
+        if (salary == null) {
+            return false;
+        }
+
+        return salaryPattern.matcher(salary).matches();
+    }
+
+    public static int convertData(String salary) {
+        return Integer.parseInt(salary);
     }
 }
